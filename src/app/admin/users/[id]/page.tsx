c50f55be47af44006a20d30bb6dcb24f2a878473
Gospel Amanze <ollygos@gmail.com>
2025-06-13 16:30:32 +0100

    still on the user Id page error

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,5 +1,5 @@
 import UserProfileClient from "@/components/user-profile-client";
 
-export default async function UserProfilePage(props: any) {
-  return <UserProfileClient userId={props.params.id} />
+export default async function UserProfilePage({ params }: { params: { id: string } }) {
+  return <UserProfileClient userId={params.id} />
 }
