d0e82f493f476526b97d003972fa3ac67a9cbbfc
Gospel Amanze <ollygos@gmail.com>
2025-06-13 21:39:03 +0100

    Removed the users route to test for a bit

--- src/app/admin/users/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-// import UserProfileClient from "@/components/user-profile-client";
-
-// export default async function UserProfilePage({ params }: { params: { id: string } }) {
-//   return <UserProfileClient userId={params.id} />
-// }
