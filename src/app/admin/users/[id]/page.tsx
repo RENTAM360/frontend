9ddcaa7bdc55e11208dae10dddcf400c3e504251
Gospel Amanze <ollygos@gmail.com>
2025-06-13 16:26:45 +0100

    Maybe this is the last fix

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,5 +1,5 @@
 import UserProfileClient from "@/components/user-profile-client";
 
-export default function UserProfilePage(props: any) {
+export default async function UserProfilePage(props: any) {
   return <UserProfileClient userId={props.params.id} />
 }
