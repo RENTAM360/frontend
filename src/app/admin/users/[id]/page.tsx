4172b67e50720e88452b7186a83ee990db79a3db
Gospel Amanze <ollygos@gmail.com>
2025-06-17 01:19:15 +0100

    fix: typescript error debug continuation

---
+++ src/app/admin/users/[id]/page.tsx
@@ -0,0 +1,5 @@
+import UserProfileClient from "@/components/user-profile-client";
+
+export default async function UserProfilePage({ params }: { params: { id: string } }) {
+  return <UserProfileClient userId={params.id} />
+}
