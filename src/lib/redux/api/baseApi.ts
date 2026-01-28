7994e6187461ffb326cc265ccb9134fec13bfecd
Gospel Amanze <ollygos@gmail.com>
2026-01-28 16:48:50 +0100

    base api error fix

--- src/lib/redux/api/baseApi.ts
+++ src/lib/redux/api/baseApi.ts
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import type { RootState } from "../store"
-import { AuthResponse } from "./authApi"
 
 const NEXT_PUBLIC_AUTH_API_URL = process.env.NEXT_PUBLIC_AUTH_API_URL as string;
 
@@ -10,8 +9,7 @@ export const baseApi = createApi({
     baseUrl: NEXT_PUBLIC_AUTH_API_URL,
     prepareHeaders: (headers, { getState }) => {
       const state = getState() as RootState
-      const auth = state.auth as AuthResponse | null
-      const token = auth?.data
+      const token = state.auth.data
 
       if (token) {
         headers.set("authorization", `Bearer ${token}`)
